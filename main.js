d12ae6c463165111cc781c804f68ebfc8bc8d3f3
yashra4j@gmail.com <yashr4j@gmail.com>
2023-01-08 05:44:24 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,19 @@
+const modal = document.querySelector('#my-modal');
+const modalBtn = document.querySelector('#modal-btn');
+const closeBtn = document.querySelector('.close');
+
+modalBtn.addEventListener('click', openModal);
+closeBtn.addEventListener('click',closeModal);
+window.addEventListener('click', outSideClick);
+
+function openModal(){
+    modal.style.display = 'block';
+}
+function closeModal(){
+    modal.style.display = 'none';
+}
+function outSideClick(){
+    if(e.target == modal){
+        modal.style.display = 'none';
+    }
+}
